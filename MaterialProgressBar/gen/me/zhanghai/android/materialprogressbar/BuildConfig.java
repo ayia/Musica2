e5a14caf790f1bbaaadbd7d8b7b3324b630e4c0d
jhon <jhon@woiri>
2016-07-20 15:56:37 +0100

    ez

---
+++ MaterialProgressBar/gen/me/zhanghai/android/materialprogressbar/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package me.zhanghai.android.materialprogressbar;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
